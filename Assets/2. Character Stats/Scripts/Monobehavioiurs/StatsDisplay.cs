f16fcac8c5c1027fb2b7e825f015e9b047a538af
โฟแฟงแตงมัก <42931734+Theerapon@users.noreply.github.com>
2021-01-07 19:39:38 +0700

    Can Show StatsCharacter (#3)
    - press "i" key for display Stats Character
    - edit StatsCharacter_SO.cs, StatsCharacter.cs, StatsDisplay.cs
    - add region Stat Increasers, Stat Reducers, Reporter

---
+++ Assets/2. Character Stats/Scripts/Monobehavioiurs/StatsDisplay.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class StatsDisplay : MonoBehaviour
+{
+    public static StatsDisplay instance;
+    [SerializeField] private CharacterStats characterStats;
+    [SerializeField] private GameObject StatsDisplayHolder;
+    [SerializeField] private TMP_Text textCoding;
+    [SerializeField] private TMP_Text textDesign;
+    [SerializeField] private TMP_Text textTesting;
+    [SerializeField] private TMP_Text textArt;
+    [SerializeField] private TMP_Text textAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            DisplayStats();
+        }
+    }
+
+    private void SetText()
+    {
+        textCoding.text = characterStats.GetStatusCoding().ToString();
+        textDesign.text = characterStats.GetStatusDesign().ToString();
+        textTesting.text = characterStats.GetStatusTest().ToString();
+        textArt.text = characterStats.GetStatusArt().ToString();
+        textAudio.text = characterStats.GetStatusAudio().ToString();
+    }
+
+    void DisplayStats()
+    {
+        if (StatsDisplayHolder.activeSelf == true)
+        {
+            StatsDisplayHolder.SetActive(false);
+        }
+        else
+        {
+            SetText();
+            StatsDisplayHolder.SetActive(true);
+        }
+    }
+}
--- Assets/2. Character Stats/Scripts/ScriptableObjects/CharacterStats_SO.cs
+++ Assets/2. Character Stats/Scripts/ScriptableObjects/CharacterStats_SO.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewStats", menuName = "Character/Stats", order = 1)]
 public class CharacterStats_SO : ScriptableObject
 {
+    [System.Serializable]
     #region jobs
-    public enum Jobs
+    private enum Jobs
     {
         STUDENT,
         PROGRAMMER,
@@ -18,19 +17,186 @@ public class CharacterStats_SO : ScriptableObject
     #endregion
 
     #region Fields
-    public string nameCharacter = "";
+    [SerializeField] private string nameCharacter = "";
 
-    public bool isPlayer = false;
+    [SerializeField] private bool isPlayer = false;
 
-    public int maxEnergy = 0;
-    public int courrentEnergy = 0;
+    [SerializeField] private int maxEnergy = 0;
+    [SerializeField] private int currentEnergy = 0;
 
-    public int codingStatus = 0;
-    public int designStatus = 0;
-    public int artStatus = 0;
-    public int audioStatus = 0;
-    public int testStatus = 0;
+    [SerializeField] private int codingStatus = 0;
+    [SerializeField] private int designStatus = 0;
+    [SerializeField] private int artStatus = 0;
+    [SerializeField] private int audioStatus = 0;
+    [SerializeField] private int testStatus = 0;
 
-    public Jobs currentJobs = Jobs.STUDENT;
+    [SerializeField] private Jobs currentJobs = Jobs.STUDENT;
+    #endregion
+
+    
+    #region Stat Increasers
+    public void ApplyMaxEnergy(int newEnergyAmount)
+    {
+        maxEnergy = newEnergyAmount;
+    }
+
+    public void ApplyCurrentEnergy(int energyAmount)
+    {
+        if ((currentEnergy + energyAmount) > maxEnergy)
+        {
+            currentEnergy = maxEnergy;
+        }
+        else
+        {
+            currentEnergy += energyAmount;
+        } 
+    }
+
+    public void ApplyCodingStatus(int codingAmount)
+    {
+        codingStatus += codingAmount;
+    }
+
+    public void ApplyDesignStatus(int designAmount)
+    {
+        designStatus += designAmount;
+    }
+
+    public void ApplyArtStatus(int artAmount)
+    {
+        artStatus += artAmount;
+    }
+
+    public void ApplyAudioStatus(int audioAmount)
+    {
+        audioStatus += audioAmount;
+    }
+
+    public void ApplyTestStatus(int testAmount)
+    {
+        testStatus += testAmount;
+    }
+    #endregion
+
+    #region Stat Reducers
+    public void TakeEnergy(int energyAmount)
+    {
+        if(currentEnergy - energyAmount <= 0)
+        {
+            currentEnergy = 0;
+        }
+        else
+        {
+            currentEnergy -= energyAmount;
+        }
+    }
+
+    public void ReducedCoding(int codingAmount)
+    {
+        if(codingStatus - codingAmount <= 0)
+        {
+            codingStatus = 0;
+        }
+        else
+        {
+            codingStatus -= codingAmount;
+        }
+    }
+
+    public void ReducedDesign(int designAmount)
+    {
+        if (designStatus - designAmount <= 0)
+        {
+            designStatus = 0;
+        }
+        else
+        {
+            designStatus -= designAmount;
+        }
+    }
+
+    public void ReducedArt(int artAmount)
+    {
+        if (artStatus - artAmount <= 0)
+        {
+            artStatus = 0;
+        }
+        else
+        {
+            artStatus -= artAmount;
+        }
+    }
+
+    public void ReducedAudio(int audioAmount)
+    {
+        if (audioStatus - audioAmount <= 0)
+        {
+            audioStatus = 0;
+        }
+        else
+        {
+            audioStatus -= audioAmount;
+        }
+    }
+
+    public void ReducedTest(int testAmount)
+    {
+        if (testStatus - testAmount <= 0)
+        {
+            testStatus = 0;
+        }
+        else
+        {
+            testStatus -= testAmount;
+        }
+    }
+    #endregion
+
+    #region Reporter
+    public string GetNameCharacter()
+    {
+        return nameCharacter;
+    }
+
+    public bool GetIsPlayer()
+    {
+        return isPlayer;
+    }
+
+    public int GetMaxEnergy()
+    {
+        return maxEnergy;
+    }
+
+    public int GetCurrentEnergy()
+    {
+        return currentEnergy;
+    }
+
+    public int GetStatusCoding()
+    {
+        return codingStatus;
+    }
+
+    public int GetStatusDesign()
+    {
+        return designStatus;
+    }
+
+    public int GetStatusArt()
+    {
+        return artStatus;
+    }
+
+    public int GetStatusTest()
+    {
+        return testStatus;
+    }
+
+    public int GetStatusAudio()
+    {
+        return audioStatus;
+    }
+
     #endregion
 }
