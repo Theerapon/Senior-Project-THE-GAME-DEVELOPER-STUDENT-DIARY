2a607f9529f250740116fee4c5b6a98ae7f332a8
Pmkbytnn <premika.mie@hotmail.com>
2021-02-04 14:46:29 +0700

    Conflict

--- Assets/07.TypingManager/Scripts/WordTimer.cs
+++ Assets/07.TypingManager/Scripts/WordTimer.cs
@@ -7,7 +7,6 @@ public class WordTimer : MonoBehaviour
     public WordManager wordManager;
 
     private float wordDelay = 4f;
-    private float time;
     private const int TIMESCALE = 1;
 
     private void Start()
