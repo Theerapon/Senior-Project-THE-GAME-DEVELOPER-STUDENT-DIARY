23078382ca0427b5323c3e5b3766f419a283004f
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-02-19 22:16:43 +0700

    Change Part Folder
    - edit stats display
    - edit menu manager

---
+++ Assets/Common/Scripts/View/InputController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class InputController : MonoBehaviour
+{
+
+    #region Defination
+    [Header("Project")]
+    [SerializeField] private GameObject _ProjectDisplayHolder;
+
+
+    [Header("Menu")]
+    [SerializeField] private GameObject _MenuHandler;
+
+    [Header("Hotbar")]
+    [SerializeField] private GameObject _HotbarHandler;
+    #endregion
+
+
+    protected void Start()
+    {
+
+        GameManager.Instance.OnGameStateChanged.AddListener(HandleGameStateChanged);
+    }
+
+    private void HandleGameStateChanged(GameManager.GameState currentState, GameManager.GameState previousState)
+    {
+        switch (currentState)
+        {
+            case GameManager.GameState.PREGAME:
+                break;
+            case GameManager.GameState.RUNNING:
+                if(_HotbarHandler.activeSelf == false)
+                {
+                    _HotbarHandler.SetActive(true);
+                }
+                break;
+            case GameManager.GameState.PAUSE:
+                if (_HotbarHandler.activeSelf == true)
+                {
+                    _HotbarHandler.SetActive(false);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Update()
+    {
+        switch (GameManager.Instance.CurrentGameState)
+        {
+            #region Pregame
+            case GameManager.GameState.PREGAME:
+                break;
+            #endregion
+            #region Running
+            case GameManager.GameState.RUNNING:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    DisplayMenu();
+                    GameManager.Instance.PuaseGame();
+
+                }
+                break;
+            #endregion
+            #region Pause
+            case GameManager.GameState.PAUSE:
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    DisplayMenu();
+                    GameManager.Instance.PuaseGame();
+                }
+                break;
+            #endregion
+            default:
+                break;
+        }
+
+    }
+
+    #region Projects
+    private void DisplayProject()
+    {
+        if (_ProjectDisplayHolder.activeSelf == true && _ProjectDisplayHolder != null)
+        {
+            _ProjectDisplayHolder.SetActive(false);
+        }
+        else
+        {
+            //SetDisplayProject();
+            _ProjectDisplayHolder.SetActive(true);
+        }
+    }
+    #endregion
+
+
+    private void DisplayMenu()
+    {
+        if (_MenuHandler.activeSelf == true && _MenuHandler != null)
+        {
+            _MenuHandler.SetActive(false);
+        }
+        else
+        {
+            _MenuHandler.SetActive(true);
+            MenuManager.Instance.Reset();
+        }
+    }
+
+    private void DisplayHotbar()
+    {
+        if (_HotbarHandler.activeSelf == true && _HotbarHandler != null)
+        {
+            _HotbarHandler.SetActive(false);
+        }
+        else
+        {
+            _HotbarHandler.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Common/Scripts/View/StatsDisplay.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class StatsDisplay : MonoBehaviour
+{
+    [SerializeField] private CharacterStats characterStats;
+    [SerializeField] private TMP_Text textCoding;
+    [SerializeField] private TMP_Text textDesign;
+    [SerializeField] private TMP_Text textTesting;
+    [SerializeField] private TMP_Text textArt;
+    [SerializeField] private TMP_Text textAudio;
+
+
+    protected void Start()
+    {
+        Debug.Log("Stats Display");
+        GameManager.Instance.OnGameStateChanged.AddListener(HandleGameStateChanged);
+        MenuManager.Instance.OnStatsShowed.AddListener(HandleStatsShowed);
+    }
+
+    private void HandleStatsShowed()
+    {
+        SetText();
+    }
+
+    private void HandleGameStateChanged(GameManager.GameState currentState, GameManager.GameState previousState)
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SetText()
+    {
+        textCoding.text = characterStats.GetStatusCoding().ToString();
+        textDesign.text = characterStats.GetStatusDesign().ToString();
+        textTesting.text = characterStats.GetStatusTest().ToString();
+        textArt.text = characterStats.GetStatusArt().ToString();
+        textAudio.text = characterStats.GetStatusAudio().ToString();
+    }
+
+}
