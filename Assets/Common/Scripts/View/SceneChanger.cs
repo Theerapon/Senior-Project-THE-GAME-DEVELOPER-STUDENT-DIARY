1214c916f536d16960dc615d28123a21d3c21f9c
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-04-16 18:05:24 +0700

    add Fade-In Fade-Out

---
+++ Assets/Common/Scripts/View/SceneChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel();
+        }
+    }
+
+    public void FadeToLevel()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        Debug.Log("Fade Complete");
+        //LoadScene
+    }
+}
