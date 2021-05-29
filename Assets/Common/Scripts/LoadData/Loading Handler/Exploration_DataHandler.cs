eee0f63bb6ffe605fde5ae989aa8d7fa8143d6fe
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-05-29 19:50:03 +0700

    add Exploration data handler

---
+++ Assets/Common/Scripts/LoadData/Loading Handler/Exploration_DataHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exploration_DataHandler : Manager<Exploration_DataHandler>
+{
+    protected Dictionary<string, Exploration_Template> explorationDic;
+    [SerializeField] private ExplorationVM explorationVM;
+    [SerializeField] private InterpretHandler interpretHandler;
+    public Dictionary<string, Exploration_Template> GetExplorationDic
+    {
+        get { return explorationDic; }
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        explorationDic = new Dictionary<string, Exploration_Template>();
+    }
+
+    private void Start()
+    {
+        interpretHandler.EventOnPreparingInterpretData.AddListener(EventInterpretHandler);
+    }
+
+    private void EventInterpretHandler()
+    {
+        explorationDic = explorationVM.Interpert();
+        //Debug.Log("activities interpret completed");
+        //foreach (KeyValuePair<string, Exploration_Template> exploration in explorationDic)
+        //{
+        //    foreach (KeyValuePair<int, List<string>> explorationDialogueIdLists in exploration.Value.ExplodialogueIdList)
+        //    {
+        //        for(int i = 0; i < explorationDialogueIdLists.Value.Count; i++)
+        //        {
+        //            Debug.Log(string.Format("ID {0}, Round {1}, Dialogue {2} = {3}",
+        //            exploration.Key, explorationDialogueIdLists.Key, i, explorationDialogueIdLists.Value[i]));
+        //        }
+
+        //    }
+
+        //}
+    }
+}
---
+++ Assets/Common/Scripts/Template/Exploration_Template.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exploration_Template : MonoBehaviour
+{
+    private string id = string.Empty;
+    private bool locked = false;
+    private int maxLevel = 5;
+    private string explorationIdRequired = string.Empty;
+    private int levelRequired = 0;
+    private List<int> expRequired = null;
+    private Dictionary<int, List<string>> explorationDialogueIdList = null;
+
+    public string Id { get => id; }
+    public bool Locked { get => locked; set => locked = value; }
+    public int MaxLevel { get => maxLevel; }
+    public string ExplorationIdRequired { get => explorationIdRequired; }
+    public int LevelRequired { get => levelRequired; }
+    public List<int> ExpRequired { get => expRequired; }
+    public Dictionary<int, List<string>> ExplodialogueIdList { get => explorationDialogueIdList; }
+
+    public Exploration_Template(string id, bool locked, int maxLevel, string explorationIdRequired, int levelRequired, List<int> expRequired, Dictionary<int, List<string>> explodialogueIdList)
+    {
+        this.id = id;
+        this.locked = locked;
+        this.maxLevel = maxLevel;
+        this.explorationIdRequired = explorationIdRequired;
+        this.levelRequired = levelRequired;
+        this.expRequired = expRequired;
+        this.explorationDialogueIdList = explodialogueIdList;
+    }
+}
