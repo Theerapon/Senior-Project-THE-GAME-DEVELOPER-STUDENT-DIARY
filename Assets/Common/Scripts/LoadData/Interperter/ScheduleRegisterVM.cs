3ea6bad30e1763645d9742809b2603c5152f05a6
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-06-01 21:40:58 +0700

    add schedule register file data, data handler, interpretVM

--- Assets/Common/Scripts/LoadData/Interperter/ScheduleRegisterVM.cs
+++ Assets/Common/Scripts/LoadData/Interperter/ScheduleRegisterVM.cs
@@ -1,18 +1,78 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ScheduleRegisterVM : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Instance
+    private const string INST_SET_ID = "ID";
+    private const string INST_SET_RegisterName = "RegisterName";
+    private const string INST_SET_DefaultRegister = "DefaultRegister";
+    private const string INST_SET_Register = "Register";
+    #endregion
+
+    [SerializeField] private ScheduleRegister_Loading scheduleRegister_Loading;
+
+    public Dictionary<string, ScheduleRegister_Template> Interpert()
     {
-        
+        if (!ReferenceEquals(scheduleRegister_Loading, null))
+        {
+            Dictionary<string, ScheduleRegister_Template> scheduleRegisterDic = new Dictionary<string, ScheduleRegister_Template>();
+
+            foreach (KeyValuePair<string, string> line in scheduleRegister_Loading.textLists)
+            {
+                ScheduleRegister_Template scheduleRegister = null;
+                string key = line.Key;
+                string value = line.Value;
+
+                scheduleRegister = CreateTemplate(value);
+
+                if (!ReferenceEquals(scheduleRegister, null))
+                {
+                    scheduleRegisterDic.Add(key, scheduleRegister);
+                }
+
+            }
+            if (!ReferenceEquals(scheduleRegisterDic, null))
+            {
+                return scheduleRegisterDic;
+            }
+        }
+
+        return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    private ScheduleRegister_Template CreateTemplate(string line)
     {
-        
+        string id = string.Empty;
+        string registerName = string.Empty;
+        bool defaultRegister = true;
+        List<string> scheduleIds = new List<string>();
+
+        string[] entries = line.Split(',');
+        for (int i = 0; i < entries.Length; i++)
+        {
+            string entry = entries[i];
+            switch (entry)
+            {
+                case INST_SET_ID:
+                    id = entries[++i];
+                    break;
+                case INST_SET_RegisterName:
+                    registerName = entries[++i];
+                    break;
+                case INST_SET_DefaultRegister:
+                    defaultRegister = bool.Parse(entries[++i]);
+                    break;
+                case INST_SET_Register:
+                    scheduleIds.Add(entries[++i]);
+                    break;
+
+            }
+
+        }
+
+        return new ScheduleRegister_Template(id, registerName, defaultRegister, scheduleIds);
     }
 }
--- Assets/Common/Scripts/LoadData/Loading Handler/ScheduleRegister_DataHandler.cs
+++ Assets/Common/Scripts/LoadData/Loading Handler/ScheduleRegister_DataHandler.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScheduleRegister_DataHandler : MonoBehaviour
+public class ScheduleRegister_DataHandler : Manager<ScheduleRegister_DataHandler>
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Dictionary<string, ScheduleRegister_Template> scheduleRegisterDic;
+    [SerializeField] private ScheduleRegisterVM scheduleRegisterVM;
+    [SerializeField] private InterpretHandler interpretHandler;
+
+
+    public Dictionary<string, ScheduleRegister_Template> GetScheduleRegisterDic
     {
-        
+        get { return scheduleRegisterDic; }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Awake()
     {
-        
+        base.Awake();
+        scheduleRegisterDic = new Dictionary<string, ScheduleRegister_Template>();
+    }
+    private void Start()
+    {
+        interpretHandler.EventOnPreparingInterpretData.AddListener(EventInterpretHandler);
+
+    }
+
+    private void EventInterpretHandler()
+    {
+        scheduleRegisterDic = scheduleRegisterVM.Interpert();
+        //Debug.Log("Schedule Rgister interpret completed");
+        //foreach (KeyValuePair<string, ScheduleRegister_Template> scheduleRegister in scheduleRegisterDic)
+        //{
+        //    for(int i = 0; i < scheduleRegister.Value.ScheduleId.Count; i++)
+        //    {
+        //        Debug.Log(string.Format("ID = {0}, Name = {1}, ScheduleID = {2}",
+        //        scheduleRegister.Key, scheduleRegister.Value.RegisterName, scheduleRegister.Value.ScheduleId[i]));
+        //    }
+
+        //}
     }
 }
